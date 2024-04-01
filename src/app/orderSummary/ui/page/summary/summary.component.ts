e8d2132ee8af5ebdac339a08386a7143e53e4f07
Kanagaraj1818 <kanagaraj199818@gmail.com>
2024-04-01 13:32:52 +0530

    observable added

--- src/app/orderSummary/ui/page/summary/summary.component.ts
+++ src/app/orderSummary/ui/page/summary/summary.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../../home/ui/features/header/header.component';
 import { FooterComponent } from '../../../../home/ui/features/footer/footer.component';
+import { HomeService } from '../../../../home/smart/data-access/home.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,CommonModule],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
 export class SummaryComponent {
-
+constructor(public service:HomeService){}
 }
