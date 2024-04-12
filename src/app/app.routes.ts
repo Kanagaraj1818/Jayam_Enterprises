0fbf117910449fee6e948099eeb2e873d71abafc
Kanagaraj1818 <kanagaraj199818@gmail.com>
2024-04-12 22:39:49 +0530

    all done about us added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/smart/entry-component/home/home.component';
 import { OrderSummaryComponent } from './orderSummary/smart/entry-component/order-summary/order-summary.component';
+import { OrdersComponent } from './orders/smart/entry-component/orders/orders.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'orders', component: OrderSummaryComponent}
+    {path: 'orders', component: OrderSummaryComponent},
+    {path: 'aboutUs', component: OrdersComponent}
 ];
--- src/app/home/smart/data-access/home.service.ts
+++ src/app/home/smart/data-access/home.service.ts
@@ -15,59 +15,59 @@ export class HomeService {
   services:ServicesList[]=[{services:[{
     name:"AC Repair",
     img:"..\\..\\..\\..\\assets\\compressed\\ac.svg",
-    background:"https://st4.depositphotos.com/1010613/24267/i/450/depositphotos_242671330-stock-photo-male-electrician-checking-air-conditioner.jpg"
+    background:"..\\..\\..\\..\\assets\\pics\\ac.jpg"
   },{
     name:"Washing M/C Repair",
     img:"..\\..\\..\\..\\assets\\compressed\\washing.svg",
-    background:"https://t3.ftcdn.net/jpg/01/98/65/94/240_F_198659488_BT7evs4AEwey5wqrllvQTZYGezk4YaCZ.jpg"
+    background:"..\\..\\..\\..\\assets\\pics\\washing.jpg"
   },{
     name:"Refrigirator Repair",
     img:"..\\..\\..\\..\\assets\\compressed\\fridge.svg",
-    background:"https://media.istockphoto.com/id/1128872850/photo/repairman-fixing-refrigerator-with-screwdriver.jpg?s=612x612&w=0&k=20&c=F6fgkzj1e_2x-diaozUqVhHuN967oY5bmlbaO5kD5Xk="
+    background:"..\\..\\..\\..\\assets\\pics\\fridge.jpg"
   }],name:"Electrical and Electronic Repair"},
   {
     services:[
       {
         name:"Electrician",
         img:"..\\..\\..\\..\\assets\\compressed\\electrician.svg",
-        background:"https://t4.ftcdn.net/jpg/01/70/17/97/240_F_170179753_piUSFjn4xlwAKaIU14gs6FnCVij2wckc.jpg"
+        background:"..\\..\\..\\..\\assets\\pics\\electrician.jpg"
       },{
         name:"CCTV Installation/Service",
         img:"..\\..\\..\\..\\assets\\compressed\\cctv.svg",
-        background:"https://t3.ftcdn.net/jpg/05/00/53/80/240_F_500538090_TOhgoTRzZVvMuqzdpimk8JZIBCB5O01Z.jpg"
+        background:"..\\..\\..\\..\\assets\\pics\\cctv.jpg"
       }
     ],name:""
   },{
     services:[{
       name:"Plumber",
       img:"..\\..\\..\\..\\assets\\compressed\\plumbing.svg",
-      background:"https://t3.ftcdn.net/jpg/03/29/58/14/240_F_329581450_931iXKUlekGxzv2rsDgRdlucndykJhkI.jpg"
+      background:"..\\..\\..\\..\\assets\\pics\\plumber.jpg"
     },{
       name:"Carpenter",
       img:"..\\..\\..\\..\\assets\\compressed\\carpenter.svg",
-      background:"https://t3.ftcdn.net/jpg/01/99/14/82/240_F_199148220_O6Bhhp46zMisAXa0pSpTxhyEUYFiyFGC.jpg"
+      background:"..\\..\\..\\..\\assets\\pics\\carpenter.jpg"
     },{
       name:"Painter",
       img:"..\\..\\..\\..\\assets\\compressed\\painter.svg",
-      background:"https://t3.ftcdn.net/jpg/01/38/71/50/240_F_138715034_3P1ZpE44DMTBcce6iOQEVZGKK5zRfRiZ.jpg"
+      background:"..\\..\\..\\..\\assets\\pics\\painter.jpg"
     }],name:"House Repair"
   },
   {
     services:[{
       name:"Civil Work",
       img:"..\\..\\..\\..\\assets\\compressed\\civil.svg",
-      background:"https://t4.ftcdn.net/jpg/06/33/49/87/240_F_633498752_gzCRsCLVTY7DIdAK1nK3HH81JDX7Dzva.jpg"
+      background:"..\\..\\..\\..\\assets\\pics\\civil.jpg"
     },{
       name:"Steel Work",
       img:"..\\..\\..\\..\\assets\\compressed\\welding.svg",
-      background:"https://t3.ftcdn.net/jpg/04/10/03/94/240_F_410039407_8fyFaq1uSrWN9ZA5OQ7yANBwUNzcL52J.jpg"
+      background:"..\\..\\..\\..\\assets\\pics\\steel.jpg"
     }],name:""
   },
   {services:[
     {
       name:"Consultancy Services",
       img:"..\\..\\..\\..\\assets\\compressed\\consultancy.svg",
-      background:"https://static.photodexia.com/original/repository/d-enblog/01490db2605b63d90313c3378471c94565c2f4399f74c/d5a625cd1825a9dbbf3745af2d1c71777dff1b6c472a76f2b5b2cd910a7b177d_65c2f4363959b.webp"
+      background:"..\\..\\..\\..\\assets\\pics\\service.webp"
     }],name:"We are Providing Consultancy Service For Entire House"}
 ]
   currentCatogory$:Observable<string>;
