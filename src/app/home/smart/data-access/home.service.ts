de39b4b20fe97d180e22a658002970bfc56073ce
Kanagaraj1818 <kanagaraj199818@gmail.com>
2024-04-14 01:51:48 +0530

    all added

--- src/app/home/smart/data-access/home.service.ts
+++ src/app/home/smart/data-access/home.service.ts
@@ -10,112 +10,124 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class HomeService {
-  catogory!:string;
-  postUrl:string="http://localhost:3000/sendmail";
-  services:ServicesList[]=[{services:[{
-    name:"AC Repair",
-    img:"..\\..\\..\\..\\assets\\compressed\\ac.svg",
-    background:"..\\..\\..\\..\\assets\\pics\\ac.jpg"
-  },{
-    name:"Washing M/C Repair",
-    img:"..\\..\\..\\..\\assets\\compressed\\washing.svg",
-    background:"..\\..\\..\\..\\assets\\pics\\washing.jpg"
-  },{
-    name:"Refrigirator Repair",
-    img:"..\\..\\..\\..\\assets\\compressed\\fridge.svg",
-    background:"..\\..\\..\\..\\assets\\pics\\fridge.jpg"
-  }],name:"Electrical and Electronic Repair"},
+  catogory!: string;
+  postUrl: string = "https://jayam-enterprises-backend.onrender.com/sendmail";
+  services: ServicesList[] = [{
+    services: [{
+      name: "AC Repair",
+      img: "..\\..\\..\\..\\assets\\compressed\\ac.svg",
+      background: "..\\..\\..\\..\\assets\\pics\\ac.jpg"
+    }, {
+      name: "Washing M/C Repair",
+      img: "..\\..\\..\\..\\assets\\compressed\\washing.svg",
+      background: "..\\..\\..\\..\\assets\\pics\\washing.jpg"
+    }, {
+      name: "Refrigirator Repair",
+      img: "..\\..\\..\\..\\assets\\compressed\\fridge.svg",
+      background: "..\\..\\..\\..\\assets\\pics\\fridge.jpg"
+    }], name: "Electrical and Electronic Repair"
+  },
   {
-    services:[
+    services: [
       {
-        name:"Electrician",
-        img:"..\\..\\..\\..\\assets\\compressed\\electrician.svg",
-        background:"..\\..\\..\\..\\assets\\pics\\electrician.jpg"
+        name: "Electrician",
+        img: "..\\..\\..\\..\\assets\\compressed\\electrician.svg",
+        background: "..\\..\\..\\..\\assets\\pics\\electrician.jpg"
+      }, {
+        name: "CCTV Installation/Service",
+        img: "..\\..\\..\\..\\assets\\compressed\\cctv.svg",
+        background: "..\\..\\..\\..\\assets\\pics\\cctv.jpg"
       },{
-        name:"CCTV Installation/Service",
-        img:"..\\..\\..\\..\\assets\\compressed\\cctv.svg",
-        background:"..\\..\\..\\..\\assets\\pics\\cctv.jpg"
+        name: "RO/Water Purifier",
+        img: "..\\..\\..\\..\\assets\\compressed\\ro.svg",
+        background: "..\\..\\..\\..\\assets\\pics\\ro.jpg"
       }
-    ],name:""
-  },{
-    services:[{
-      name:"Plumber",
-      img:"..\\..\\..\\..\\assets\\compressed\\plumbing.svg",
-      background:"..\\..\\..\\..\\assets\\pics\\plumber.jpg"
-    },{
-      name:"Carpenter",
-      img:"..\\..\\..\\..\\assets\\compressed\\carpenter.svg",
-      background:"..\\..\\..\\..\\assets\\pics\\carpenter.jpg"
-    },{
-      name:"Painter",
-      img:"..\\..\\..\\..\\assets\\compressed\\painter.svg",
-      background:"..\\..\\..\\..\\assets\\pics\\painter.jpg"
-    }],name:"House Repair"
+    ], name: ""
+  }, {
+    services: [{
+      name: "Plumber",
+      img: "..\\..\\..\\..\\assets\\compressed\\plumbing.svg",
+      background: "..\\..\\..\\..\\assets\\pics\\plumber.jpg"
+    }, {
+      name: "Carpenter",
+      img: "..\\..\\..\\..\\assets\\compressed\\carpenter.svg",
+      background: "..\\..\\..\\..\\assets\\pics\\carpenter.jpg"
+    }, {
+      name: "Painter",
+      img: "..\\..\\..\\..\\assets\\compressed\\painter.svg",
+      background: "..\\..\\..\\..\\assets\\pics\\painter.jpg"
+    }], name: "House Repair"
   },
   {
-    services:[{
-      name:"Civil Work",
-      img:"..\\..\\..\\..\\assets\\compressed\\civil.svg",
-      background:"..\\..\\..\\..\\assets\\pics\\civil.jpg"
+    services: [{
+      name: "Civil Work",
+      img: "..\\..\\..\\..\\assets\\compressed\\civil.svg",
+      background: "..\\..\\..\\..\\assets\\pics\\civil.jpg"
+    }, {
+      name: "Steel Work",
+      img: "..\\..\\..\\..\\assets\\compressed\\welding.svg",
+      background: "..\\..\\..\\..\\assets\\pics\\steel.jpg"
     },{
-      name:"Steel Work",
-      img:"..\\..\\..\\..\\assets\\compressed\\welding.svg",
-      background:"..\\..\\..\\..\\assets\\pics\\steel.jpg"
-    }],name:""
+      name: "Compressor Motor Repair",
+      img: "..\\..\\..\\..\\assets\\compressed\\compressor.svg",
+      background: "..\\..\\..\\..\\assets\\pics\\motor.jpg"
+    }], name: ""
   },
-  {services:[
-    {
-      name:"Consultancy Services",
-      img:"..\\..\\..\\..\\assets\\compressed\\consultancy.svg",
-      background:"..\\..\\..\\..\\assets\\pics\\service.webp"
-    }],name:"We are Providing Consultancy Service For Entire House"}
-]
-  currentCatogory$:Observable<string>;
+  {
+    services: [
+      {
+        name: "Consultancy Services",
+        img: "..\\..\\..\\..\\assets\\compressed\\consultancy.svg",
+        background: "..\\..\\..\\..\\assets\\pics\\service.webp"
+      }], name: "We are Providing Consultancy Service For Entire House"
+  }
+  ]
+  currentCatogory$: Observable<string>;
   bookForm = new FormGroup({
     name: new FormControl(null, [Validators.required, RegexValidator(RegexConstants.Name)]),
     phoneNumber: new FormControl(null, [Validators.required, RegexValidator(RegexConstants.phoneNumber)])
   });
-  constructor(private router:Router,private http:HttpClient) {
+  constructor(private router: Router, private http: HttpClient) {
     this.currentCatogory$ = new BehaviorSubject('');
-    if(sessionStorage.getItem('catogory')){
-      let catogory =  sessionStorage.getItem('catogory');
-      if(catogory){
+    if (sessionStorage.getItem('catogory')) {
+      let catogory = sessionStorage.getItem('catogory');
+      if (catogory) {
         this.catogory = catogory;
         this.currentCatogory$ = new BehaviorSubject(catogory);
-      }     
+      }
     }
     else router.navigateByUrl('/home');
-   }
-   Response:boolean = false;
-   load:boolean = false;
-   
-  selectedCatagory(value:string):void{
+  }
+  Response: boolean = false;
+  load: boolean = false;
+
+  selectedCatagory(value: string): void {
     this.currentCatogory$ = new BehaviorSubject(value);
     sessionStorage.setItem('catogory', value);
     this.router.navigateByUrl("/orders");
   }
-  mailPost(name:string,number:string){
+  mailPost(name: string, number: string) {
     this.currentCatogory$.subscribe((data) => {
       this.catogory = data
     })
-    if(this.catogory){
-      let post:Post = {
-        name :name,
-        phoneNumber:number,
-        catogory : this.catogory
+    if (this.catogory) {
+      let post: Post = {
+        name: name,
+        phoneNumber: number,
+        catogory: this.catogory
       }
-      this.load=true;
-      this.http.post<Data>(this.postUrl, post).subscribe((data)=> {
-        if(data.accepted)
+      this.load = true;
+      this.http.post<Data>(this.postUrl, post).subscribe((data) => {
+        if (data.accepted)
           this.Response = true;
-           this.load=false;
-      } 
+        this.load = false;
+      }
       )
-    }   
+    }
   }
 
-  searchText!:string;
-  onKey(value:string){
+  searchText!: string;
+  onKey(value: string) {
     this.searchText = value;
   }
 }
